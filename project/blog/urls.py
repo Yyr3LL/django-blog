01e89f51db944efcd145b37496248df452f536fb
Yyr3LL <tarasenko.yura.dr@gmail.com>
2019-06-17 10:43:17 +0300

    asd

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'', views.post_list),
+	url(r'', views.main_page, name="home"),
+	url(r'post_list/', views.post_list, name="post_list"),
 ]
 
--- project/blog/views.py
+++ project/blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Post
 
 
+def main_page(request):
+	l = Post.objects.all()
+	return render(request, 'index.html', {'list': l})
+
 def post_list(request):
     p = Post.objects.all()
-    return render(request, 'index.html', {'post': p})
+    return render(request, 'posts.html', {'post': p})
